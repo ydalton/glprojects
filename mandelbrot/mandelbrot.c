3759add7fdb34f551865fca761ccf6c9d4350ffa
ydalton <r0930164@student.thomasmore.be>
2023-01-31 14:13:12 +0100

    Added nice colors to the Mandelbrot program.

--- mandelbrot/mandelbrot.c
+++ mandelbrot/mandelbrot.c
@@ -4,24 +4,29 @@
 
 #include "drawshape.h"
 
-#define W_WIDTH 1000 
+#define W_WIDTH 1000
 #define W_HEIGHT W_WIDTH
 #define W_CENTER W_WIDTH/2, W_HEIGHT/2
 #define W_NAME "Mandelbrot"
 
-#define MAX_ITER 500
+#ifdef ZOOM
+#define ZOOM_FACTOR 0.99
+#endif
 
-#define CENTER_REAL -1.4
-#define CENTER_IMAGINARY 0.0
-#define DIAMETER 0.05
+#define MAX_ITER 50
 
-const double minr = CENTER_REAL - DIAMETER * 0.5;
-const double mini = CENTER_IMAGINARY - DIAMETER * 0.5;
-const double maxr = CENTER_REAL + DIAMETER * 0.5;
-const double maxi = CENTER_IMAGINARY + DIAMETER * 0.5;
+/* initial conditions */
+double cre = -0.5;
+double cim = 0;
+double diam = 3;
 
-static const double stepr = (maxr - minr) / W_WIDTH;
-static const double stepi = (maxi - mini) / W_HEIGHT;
+double minr;
+double mini;
+double maxr;
+double maxi;
+
+double stepr;
+double stepi;
 
 int iterate(double x, double y, uint16_t max_iter) {
     int iter;
@@ -40,22 +45,40 @@ int iterate(double x, double y, uint16_t max_iter) {
 void display(void) {
     uint16_t x, y, iter;
     int c;
-    glClear(GL_COLOR_BUFFER_BIT);
+#ifdef ZOOM
+    while(1) {
+#endif
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        minr = cre - diam * 0.5;
+        mini = cim - diam * 0.5;
+        maxr = cre + diam * 0.5;
+        maxi = cim + diam * 0.5;
+
+        stepr = (maxr - minr) / W_WIDTH;
+        stepi = (maxi - mini) / W_HEIGHT;
+
 #ifndef DO_ITERATIONS
-    iter = MAX_ITER;
+        iter = MAX_ITER;
 #endif
 #ifdef DO_ITERATIONS
-    for(iter = 0; iter < MAX_ITER; iter++) {
+        for(iter = 0; iter < MAX_ITER; iter++) {
 #endif
-        for(y = 0; y < W_HEIGHT; y++) {
-            for(x = 0; x < W_WIDTH; x++) {
-                c = iterate(minr+x*stepr, mini+y*stepi, iter);
-                //if (c == iter) c = MAX_ITER;
-                drawPixel(x, y, 255-(255*c/iter), 255-(255*c/iter), 255-(255*c/iter));
+            for(y = 0; y < W_HEIGHT; y++) {
+                for(x = 0; x < W_WIDTH; x++) {
+                    c = iterate(minr+x*stepr, mini+y*stepi, iter);
+                    if (c == iter)
+                        drawPixel(x, y, BLACK);
+                    else
+                        drawPixel(x, y, (255*c/iter), (255*c/iter), 255);
+                }
             }
-        }
-        glutSwapBuffers();
+            glutSwapBuffers();
 #ifdef DO_ITERATIONS
+        }
+#endif
+#ifdef ZOOM
+    diam *= ZOOM_FACTOR;
     }
 #endif
 }
